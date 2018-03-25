bf1593ba9dfa6140f4c21b74c1a253942c306aab
kein0r <git@dummgebabbel.de>
2018-03-24 20:52:57 -0400

    Added WiFi and MQTT init

---
+++ Esp8266TemperaturHumiditySensor/src/Esp8266TemperaturHumiditySensor_cfg.h
@@ -0,0 +1,16 @@
+/**
+ * Wifi related configuration
+*/
+#define WIFI_SSID       "TestWifi"
+#define WIFI_PASSWORD   "TestWifiPassword"
+
+/**
+ * DHT sensor configuration
+ * DHT_TYPE:
+ * - DHT11
+ * - DHT 21 (AM2301)
+ * - DHT 22 (AM2302)
+ * DHT_PIN: I/O pin which is connected to the DHT sensor
+*/
+#define DHT_TYPE        DHT22
+#define DHT_PIN         2
