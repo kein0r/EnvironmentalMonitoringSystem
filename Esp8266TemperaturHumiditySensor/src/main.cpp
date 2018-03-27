fe7734f2dfa43976ec68c1cbefba64c682dc3a11
kein0r <git@dummgebabbel.de>
2018-03-26 21:39:28 -0400

    Code clean-up

--- Esp8266TemperaturHumiditySensor/src/main.cpp
+++ Esp8266TemperaturHumiditySensor/src/main.cpp
@@ -5,39 +5,51 @@
 #include <DHT_U.h>
 #include "Esp8266TemperaturHumiditySensor_cfg.h"
 
-const char* ssid = WIFI_SSID;
-const char* password =  WIFI_PASSWORD;
-const char* mqttServer = "m11.cloudmqtt.com";
-const int mqttPort = 12948;
-
 WiFiClient espClient;
 PubSubClient client(espClient);
 
 DHT_Unified dht(DHT_PIN, DHT_TYPE);
 
+/**
+ * Connect or re-connect to mqtt broker.
+ * Note: This function is blocking! It will wait until a connection to the
+ * mqtt broker can be established.
+ */
 void reconnectMQTTBroker()
 {
-  Serial.print("Connecting to MQTT broker ... ")
   while (!client.connected())
   {
-    if (client.connect("ESP8266Client"))
+    Serial.print("Connecting to MQTT broker ... ");
+    if (client.connect(MQTT_CLIENTID))
     {
+      /* Publish some information about the module */
+      client.publish(ANNOUNCE_ADDRESSTOPIC, WiFi.localIP().toString().c_str());
       Serial.println("connected!");
     }
     else
     {
       Serial.print("failed! State: ");
       Serial.print(client.state());
-      delay(2000);
+      delay(MQTT_RECONNECTTIMER);
     }
   }
+}
+
+void mqttCallback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i=0;i<length;i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
 }
 
 void setup() {
   Serial.begin(115200);
 
   /* Connect to WiFi, wait until connection was established */
-  WiFi.begin(ssid, password);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to WiFi ");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -48,10 +60,36 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   /* Connect to mqtt broker */
-  IPAddress server(10, 0, 0, 12);
-  client.setServer(mqtt_server, 1883);
+  client.setServer(MQTT_BROKERADDRESS, MQTT_BROKERPORT);
+  client.setCallback(mqttCallback);
+
+  /* Init Sensors */
+  dht.begin();
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+    reconnectMQTTBroker();
+
+    sensors_event_t event;
+    char sensorString[20];
+    dht.temperature().getEvent(&event);
+    if (isnan(event.temperature)) {
+      Serial.println("Error reading temperature!");
+    }
+    else {
+      sprintf(sensorString, "%2.2f", event.temperature);
+      client.publish(SENSOR_TEMPERATURETOPIC, sensorString);
+    }
+    /* Note: As long as we stay below minimum sample rate the sensor will not be
+     * read again but just the values from the temperature reading above will
+     * will be used. */
+    dht.humidity().getEvent(&event);
+    if (isnan(event.relative_humidity)) {
+      Serial.println("Error reading humidity!");
+    }
+    else {
+      sprintf(sensorString, "%2.2f", event.relative_humidity);
+      client.publish(SENSOR_HUMIDITYTOPIC, sensorString);
+    }
+    delay(SENSOR_MEASUREMENTTIMER);
 }
