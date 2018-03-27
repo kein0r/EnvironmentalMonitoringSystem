78343ec74ebcde3bf88f8369ec26d35c04675455
kein0r <git@dummgebabbel.de>
2018-03-26 21:39:28 -0400

    Code clean-up

--- Esp8266TemperaturHumiditySensor/src/Esp8266TemperaturHumiditySensor_cfg.h
+++ Esp8266TemperaturHumiditySensor/src/Esp8266TemperaturHumiditySensor_cfg.h
@@ -1,8 +1,34 @@
 /**
  * Wifi related configuration
+ * Only set if they're not already set in platformio.ini using
+ * build_flags = -DWIFI_SSID=\"AnotherTestWifi\" -DWIFI_PASSWORD=\"NewPassword\"
 */
+#ifndef WIFI_SSID
 #define WIFI_SSID       "TestWifi"
+#endif
+#ifndef WIFI_PASSWORD
 #define WIFI_PASSWORD   "TestWifiPassword"
+#endif
+
+/**
+ * MQTT_CLIENTID. Id to be sent to the broker to identify the client.
+ * Note: Can be overwritten using build_flags
+ */
+#ifndef MQTT_CLIENTID
+#define MQTT_CLIENTID   "ESP8266Client"
+#endif
+
+/**
+ * MQTT configuration
+ * MQTT_BROKERADDRESS either a string "whatever.mqttbrokr.com" or an ip IPAddress
+ * IPAddress(192, 168, 0, 55)
+ * MQTT_BROKERPORT port used to connect to
+ * MQTT_RECONNECTTIMER time between two connection attempts in case connection
+ * can't be estatblished or is lost
+ */
+#define MQTT_BROKERADDRESS      IPAddress(10, 0, 0, 12)
+#define MQTT_BROKERPORT         (int)1883
+#define MQTT_RECONNECTTIMER     (unsigned long)5000
 
 /**
  * DHT sensor configuration
@@ -14,3 +40,20 @@
 */
 #define DHT_TYPE        DHT22
 #define DHT_PIN         2
+
+/**
+ * Topics used by this sensor (all lower case)
+ * SENSOR_HUMIDITYTOPIC and SENSOR_TEMPERATURETOPIC humidity and temperature to
+ * be send to the MQTT broker
+ * SENSOR_LEDTOPIC to subscribe to, to toggle the LED
+ */
+#define SENSOR_TEMPERATURETOPIC "home/groundfloor/diningroom/temperature"
+#define SENSOR_HUMIDITYTOPIC    "home/groundfloor/diningroom/humidity"
+#define SENSOR_LEDTOPIC         "home/groundfloor/diningroom/led"
+#define SENSOR_MEASUREMENTTIMER (unsigned long)(2*60*1000)
+
+/**
+ *
+ */
+#define ANNOUNCE_ADDRESSTOPIC   "home/clients/address"
+#define ANNOUNCE_TOPICSTOPIC    "home/clients/topics"
