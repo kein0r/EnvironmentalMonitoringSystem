e0559a2beece49647930d789061af6c2f44940f9
kein0r <github@dummgebabbel.de>
2018-12-30 11:17:49 -0500

    Added reception function - adapted PINs to match hardware

--- AMREnergyMonitor/include/AMREnergyMonitor.h
+++ AMREnergyMonitor/include/AMREnergyMonitor.h
@@ -1,9 +1,22 @@
-#ifndef AMRENERYMONITOR_H
-#define AMRENERYMONITOR_H
+#ifndef AMRENERGYMONITOR_H
+#define AMRENERGYMONITOR_H
 
-#define RFM69_CS      2    // "E"
-#define RFM69_IRQ     15   // "B"
-#define RFM69_RST     16   // "D"
+/**
+ * Center frequency in MHz. 240.0 to 960.0
+ */
+#define RF69_FREQ         (float)915.0
+
+/**
+ * Define TX power. Range from 14-20
+ */
+#define RF69_TXPOWER      (int8_t)20
+
+/**
+ * Define pins used to connect to RF module
+ */
+#define RFM69_CS      15   // "E"
+#define RFM69_IRQ     4    // G0 on rf board
+#define RFM69_RST     2    // "D"
 #define LED           0
 
 #endif
