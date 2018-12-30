2c30eba23d08de67c4b1abe11b969688086e751c
kein0r <git@dummgebabbel.de>
2018-12-30 15:31:20 -0500

    Added "normal" OTA for now for faster debugging (to be removed later)

--- AMREnergyMonitor/src/main.cpp
+++ AMREnergyMonitor/src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include "AMREnergyMonitor.h"
+#include <ArduinoOTA.h>
 #include <Homie.h>
+#include <SPI.h>
 #include <RH_RF69.h>
 #include <ESP8266mDNS.h>
 
@@ -45,6 +47,25 @@ void setupHandler() {
     rf69.setTxPower(20, true);  // range from 14-20 for power, 2nd arg must be true for 69HCW
     Serial.println("RFM69 frequency set");
   }
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
 }
 
 void loopHandler()
@@ -55,7 +76,8 @@ void loopHandler()
     statusLEDState = (statusLEDState == HIGH) ? LOW : HIGH;
     nextStatusTime = millis() + 1000;
   }
-  if (rf69.available()) {
+  if (rf69.available())
+  {
     // Should be a message for us now
     uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
     uint8_t len = sizeof(buf);
@@ -80,6 +102,7 @@ void loopHandler()
       Serial.println("Receive failed");
     }
   }
+  ArduinoOTA.handle();
 }
 
 void setup()
