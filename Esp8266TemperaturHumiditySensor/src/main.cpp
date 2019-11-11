018d01da3642dda41f189d0f5a7eab40495e1616
kein0r <github@dummgebabbel.de>
2019-11-10 20:28:46 -0500

    Added OTA

--- Esp8266TemperaturHumiditySensor/src/main.cpp
+++ Esp8266TemperaturHumiditySensor/src/main.cpp
@@ -11,6 +11,7 @@
 #include <ESP8266WiFi.h>
 #include "Esp8266TemperaturHumiditySensor.h"
 #include <ESP8266mDNS.h>
+#include <ArduinoOTA.h>
 #ifdef DHT_TYPE
 #include <Adafruit_Sensor.h>
 #include <DHT_U.h>
@@ -64,6 +65,27 @@ void setup() {
   }
   //Start mDNS with name esp8266
   MDNS.begin(MQTT_CLIENTID);
+
+  // Start OTA
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+
   Serial.println(" connected");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
@@ -91,8 +113,12 @@ void setup() {
   Serial.println(lamp.id);
 }
 
-void loop2() {
+void loop() {
   static unsigned long nextSensorRun = 0;
+
+  ArduinoOTA.handle();
+  adapter->update();
+
   if (millis() > nextSensorRun)
   {
     nextSensorRun = millis() + SENSOR_MEASUREMENTTIMER;
@@ -155,8 +181,4 @@ void loop2() {
       //humidityNode.setProperty("percentage").send(sensorString);
     }
   }
-}
-
-void loop(){
-  adapter->update();
 }
