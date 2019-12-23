4c209fa34e60357254304992266627d4479ba751
kein0r <github@dummgebabbel.de>
2019-12-22 23:09:56 -0500

    Complete code for model customization

--- DogMonitor/modelCustomization/modelCustomization.py
+++ DogMonitor/modelCustomization/modelCustomization.py
@@ -1,3 +1,6 @@
+#
+# Example taken from https://github.com/tensorflow/examples/blob/master/tensorflow_examples/lite/model_customization/demo/image_classification.ipynb
+#
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import numpy as np
@@ -11,3 +14,9 @@ from tensorflow_examples.lite.model_customization.core.task.model_spec import ef
 from tensorflow_examples.lite.model_customization.core.task.model_spec import ImageModelSpec
 
 import matplotlib.pyplot as plt
+
+#image_path = tf.keras.utils.get_file('dogs', 'images/dogs.tgz', untar=True)
+data = ImageClassifierDataLoader.from_folder("images")
+model = image_classifier.create(data)
+loss, accuracy = model.evaluate()
+model.export('dogs_classifier.tflite', 'dog_labels.txt')
