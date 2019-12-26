5c83d4ee7f75a7c95f6a9f13b5dba185a547ef61
kein0r <github@dummgebabbel.de>
2019-12-26 12:33:22 -0500

    Added external config file to store thing like telegram token etc.

--- DogMonitor/dogMonitor/dogMonitor.py
+++ DogMonitor/dogMonitor/dogMonitor.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 
 import logging
+#import PiCamera
 
 # Because filename and class name are identical
 from TelegramBot import TelegramBot
 
+# Import token from config file
+import config
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-telegramBot = TelegramBot("")
+
+def main():
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+    telegramBot = TelegramBot(telegramBotToken)
+
+
+if __name__ == '__main__':
+  main()
