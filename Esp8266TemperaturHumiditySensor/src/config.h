3730a444cc8c07f7c0f7fe9941449dabb14dd85c
kein0r <github@dummgebabbel.de>
2020-04-06 17:48:49 -0400

    Updated to ArduinoJson 6

--- Esp8266TemperaturHumiditySensor/src/config.h
+++ Esp8266TemperaturHumiditySensor/src/config.h
@@ -2,6 +2,12 @@
 #define CONFIG_H
 
 /**
+ * Sizeof the json buffer during write. Not entirely sure how this is to be
+ * calculated.
+ */
+#define JSONDOCUMENT_SIZE (int)200
+
+/**
  * Max characters for sensor sensorLocation
  */
 #define MAX_SENSOR_LOCATION_LENGTH    (int)40
