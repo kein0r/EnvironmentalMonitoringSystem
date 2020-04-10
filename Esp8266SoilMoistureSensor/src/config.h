381518bc2b77fe7cedeaaefb2cf0e4a6b09fabec
kein0r <github@dummgebabbel.de>
2020-04-10 17:11:41 -0400

    Updated new config values for sensor calibration

--- Esp8266SoilMoistureSensor/src/config.h
+++ Esp8266SoilMoistureSensor/src/config.h
@@ -2,6 +2,13 @@
 #define CONFIG_H
 
 /**
+ * All configuration related variables and definitions should go in
+ * this file
+ */
+
+#define DEBUG
+
+/**
  * Sizeof the json buffer during write. Not entirely sure how this is to be
  * calculated.
  */
@@ -11,8 +18,17 @@
  * Max characters for sensor sensorLocation
  */
 #define MAX_SENSOR_LOCATION_LENGTH    (int)40
-
 extern char thingLocationName[MAX_SENSOR_LOCATION_LENGTH];
+
+/**
+ * Default min/max values without calibration done
+ * Calibration is done by placing the dry sensor in air and then
+ * putting covering it in water.
+ */
+#define CALIBRATION_AIR_MAX         (int)1024
+#define CALIBRATION_WATER_MIN       (int)0
+extern int calibrationAir;
+extern int calibrationWater;
 
 void saveConfigCallback();
 bool readConfigValues();
